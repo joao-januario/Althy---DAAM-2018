f281fb3bc4e1c2ef888c3f7d410dd724572fc5da
DESKTOP-CTII71O\cjclc <cjclcjcl@gmail.com>
2018-04-24 16:50:05 +0100

    changes in layouts 2

--- app/src/main/java/inducesmile/com/test_real_time/AppNav/ChooseCatg.java
+++ app/src/main/java/inducesmile/com/test_real_time/AppNav/ChooseCatg.java
@@ -2,14 +2,52 @@ package inducesmile.com.test_real_time.AppNav;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import inducesmile.com.test_real_time.R;
 
 public class ChooseCatg extends AppCompatActivity {
 
+    public Button inc_Rounds;
+    public Button dec_Rounds;
+    public TextView rounds_tv;
+    public int rounds_count =0;
+    public  final int ROUNDS_MAX = 10;
+    public final int ROUNDS_MIN = 1;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_catg);
+
+        inc_Rounds = findViewById(R.id.btn_increaseRound);
+        inc_Rounds = findViewById(R.id.btn_decreaseRound);
+        rounds_tv = findViewById(R.id.round_tv);
+
+        inc_Rounds.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                if(rounds_count<ROUNDS_MAX)
+                    increaseRounds();
+            }
+        });
+        dec_Rounds.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                if(rounds_count>ROUNDS_MIN)
+                    decreaseRounds();
+            }
+        });
+
+    }
+
+    private void increaseRounds() {
+        rounds_count++;
+        rounds_tv.setText(rounds_count);
+    }
+    private void decreaseRounds() {
+        rounds_count--;
+        rounds_tv.setText(rounds_count);
     }
 }
